e632b762eb930af83ac48aaf48363bdc390e78c7
Rafael Fernández <rafa.fdz.flores@gmail.com>
2024-11-17 20:00:15 +0100

    Deleted console log

--- src/handlers/usersHandler.ts
+++ src/handlers/usersHandler.ts
@@ -190,8 +190,6 @@ export const getUsers = async (_req: express.Request, res: express.Response) => 
         password = (await classroomDAO.get(id_classroom)).password;
       } catch(e) {}
 
-      console.log('-----> PASSWORD: ', password)
-
       if(password !== "")
       {
         if(!body.password || password !== body.password) 
